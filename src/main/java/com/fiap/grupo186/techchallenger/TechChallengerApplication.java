a49d4b4676f4d37303b3cb73e767bde1452a1f44
Rafael Moreira <rafaelsmoreiras@gmail.com>
2025-05-14 02:20:28 -0300

    chore: this is the first commit

---
+++ src/main/java/com/fiap/grupo186/techchallenger/TechChallengerApplication.java
@@ -0,0 +1,12 @@
+package com.fiap.grupo186.techchallenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechChallengerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TechChallengerApplication.class, args);
+    }
+
+}
