165587b224c62cdfdeea1851adc9ddc059081c02
Rafael Moreira <rafaelsmoreiras@gmail.com>
2025-05-25 02:18:55 -0300

    chore: updated dtos

--- src/main/java/com/fiap/grupo186/techchallenge/application/dtos/ItemDTO.java
+++ src/main/java/com/fiap/grupo186/techchallenge/application/dtos/ItemDTO.java
@@ -1,42 +1,26 @@
 package com.fiap.grupo186.techchallenge.application.dtos;
 
 import com.fiap.grupo186.techchallenge.domains.kitchen.models.Item;
-import com.fiap.grupo186.techchallenge.domains.kitchen.models.Item.Product.ProductCategory;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 public record ItemDTO(
     @NotNull(message = "Item must not be null")
     @Size(min = 1, message = "Must have at least one item")
     int quantity,
     @NotNull(message = "Category must not be null")
-    ProductCategory category,
+    Item.ProductCategory category,
     @NotNull(message = "Price is required")
     @DecimalMin(value = "1.0", message = "Price must be greater than or equal to 1")
     BigDecimal unitPrice,
+    @NotNull(message = "ProductID not be null")
+    UUID productId,
     String customerNote,
+    @NotNull(message = "Product name must not be null")
+    String productName
+) {}
 
-    @NotNull(message = "Product must not be null")
-    ProductDTO product
-) {
-    public ItemDTO {
-        if (quantity <= 0) {
-            throw new IllegalArgumentException("Quantity must be greater than 0");
-        }
-    }
-
-    public Item.Product makeProductDTO() {
-        return new Item.Product(
-                product.id(),
-                product.name(),
-                product.description(),
-                product.price(),
-                product.isAvailable(),
-                product.category(),
-                product.createdAt()
-        );
-    }
-}
