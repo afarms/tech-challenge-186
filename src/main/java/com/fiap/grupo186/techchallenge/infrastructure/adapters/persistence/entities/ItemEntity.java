485db2abbcf135abab8f3e8207c5bf1f084ee136
Rafael Moreira <rafaelsmoreiras@gmail.com>
2025-05-25 02:18:13 -0300

    build: added jpa and hibernate dependencie

---
+++ src/main/java/com/fiap/grupo186/techchallenge/infrastructure/adapters/persistence/entities/ItemEntity.java
@@ -0,0 +1,2 @@
+package com.fiap.grupo186.techchallenge.infrastructure.adapters.persistence.entities;public class ItemEntity {
+}
---
+++ src/main/java/com/fiap/grupo186/techchallenge/infrastructure/adapters/persistence/mappers/OrderMapper.java
@@ -0,0 +1,2 @@
+package com.fiap.grupo186.techchallenge.infrastructure.adapters.persistence.mappers;public class OrderMapper {
+}
